d5c5e538bbb65d61d72e2574182ad0590a01525b
Student Ana Collage <jaiswaldebu585@gmail.com>
2025-05-17 20:18:10 +0530

    new

--- server/index.js
+++ server/index.js
@@ -45,8 +45,8 @@ if (process.env.NODE_ENV !== 'test') {
     })
     .then(() => {
       console.log("Connected to MongoDB");
-      app.listen(PORT, () => {
-        console.log(`Server running on http://localhost:${PORT}`);
+      app.listen(PORT, '0.0.0.0', () => {
+        console.log(`Server running on http://0.0.0.0:${PORT}`);
       });
     })
     .catch((err) => {
